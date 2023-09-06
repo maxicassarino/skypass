a70b85a3d281b7ef22341c79c692b47a162e3e1d
maxicassarino <maximocassarino@gmail.com>
2023-09-05 23:59:27 -0300

    version 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,67 @@
-const destinos = [{Lugar: "New York", Pasaje: 1000000, Pasajeros: 1, Recargo: "21%", Total: 0},
-                {Lugar: "Madrid", Pasaje: 750000, Pasajeros: 1, Recargo: "21%", Total: 0},
-                {Lugar: "Miami", Pasaje: 600000, Pasajeros: 1, Recargo: "21%", Total: 0},
-                {Lugar: "Londres", Pasaje: 800000, Pasajeros: 1, Recargo: "21%", Total: 0},
-                {Lugar: "Aruba", Pasaje: 500000, Pasajeros: 1, Recargo: "21%", Total: 0},
-                {Lugar: "Tokio", Pasaje: 600000, Pasajeros: 1, Recargo: "21%", Total: 0}
+// OJBETO VUELOS
+
+const vuelos = [{Lugar: "New York", Pasaje: 1095, Pasajeros: 1, Recargo: "21%", Total: 0},
+                {Lugar: "Madrid", Pasaje: 1500, Pasajeros: 1, Recargo: "21%", Total: 0},
+                {Lugar: "Miami", Pasaje: 950, Pasajeros: 1, Recargo: "21%", Total: 0},
+                {Lugar: "Londres", Pasaje: 1750, Pasajeros: 1, Recargo: "21%", Total: 0},
+                {Lugar: "Aruba", Pasaje: 800, Pasajeros: 1, Recargo: "21%", Total: 0},
+                {Lugar: "Tokio", Pasaje: 2200, Pasajeros: 1, Recargo: "21%", Total: 0}
 ]
 
 
+// ENTRADA DE DATOS
 
 alert(`Bienvenido! Inicie su consulta`);
 let eleccion = prompt("Ingrese su destino de interés");
 let pasajeros = parseInt(prompt("Ingrese la cantidad de pasajeros"))
 
 
-const cantidad = destinos.map(cantidad => cantidad.Pasajeros = pasajeros)
-const valorTotal = destinos.map(valorTotal => valorTotal.Total = (valorTotal.Pasaje * pasajeros) * 1.21)
-const buscarDestino = destinos.find(busqueda => busqueda.Lugar === eleccion)
+// MODIFICACION DE DATOS
 
-console.log(buscarDestino);
+const cantidad = vuelos.map(cantidad => cantidad.Pasajeros = pasajeros)
+const valorTotal = vuelos.map(valorTotal => valorTotal.Total = (valorTotal.Pasaje * pasajeros) * 1.21)
+const buscarVuelo = vuelos.find(busqueda => busqueda.Lugar === eleccion)
+
+console.log(buscarVuelo);
 
 
+// MENSAJES EN STORAGE
 
 localStorage.setItem(`Bienvenida`, `Hola Pasajeros!`)
 
+localStorage.setItem("Vuelo Seleccionado", JSON.stringify(buscarVuelo))
 
-localStorage.setItem("Paquete Seleccionado", JSON.stringify(buscarDestino));
-
-
-for (const destino of destinos) {
-    sessionStorage.setItem(destino.Lugar, destino.Total)
-};
-
-localStorage.setItem(`Despedida`, `Buen Viaje!`)
+for (const vuelo of vuelos) {
+    sessionStorage.setItem(vuelo.Lugar, vuelo.Total)
+}
 
 
+// HTML DESTINO ELEGIDO
 
+let foto = eleccion
+
+if (eleccion == "New York") {
+    foto = "ny"
+}
+
+const main = document.getElementsByTagName("main")[0]
+
+let section = document.createElement("section");
+section.id = "destino"
+section.innerHTML =    `<img src="assets/images/Inicio/ciudades/${foto}.jpg" alt="">
+                        <div id="datos">
+                            <h2>${eleccion}</h2>
+                            <div id="listas">
+                                <ul id="lista">
+                                    <li>Pasaje</li>
+                                    <li>Impuestos</li>
+                                    <li>Total</li>
+                                </ul>
+                                <ul id="lista2">
+                                    <li>${buscarVuelo.Pasaje} USD</li>
+                                    <li>21%</li>
+                                    <li>${buscarVuelo.Total} USD</li>
+                                </ul>
+                            </div>
+                        </div>`;
+main.appendChild(section);
